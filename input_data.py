f84d0866b1bcca3516b7672b0b5a1e09d31de85d
tohnperfect <thanapong.intharah@gmail.com>
2016-12-05 22:03:11 +0000

    make it runable with python2&3

--- input_data.py
+++ input_data.py
@@ -2,7 +2,12 @@
 from __future__ import print_function
 import gzip
 import os
-import urllib
+import sys
+if sys.version_info[0] >= 3:
+  from urllib.request import urlretrieve
+else:
+  from urllib import urlretrieve
+#import urllib
 import numpy
 SOURCE_URL = 'http://yann.lecun.com/exdb/mnist/'
 def maybe_download(filename, work_directory):
@@ -11,7 +16,7 @@ def maybe_download(filename, work_directory):
     os.mkdir(work_directory)
   filepath = os.path.join(work_directory, filename)
   if not os.path.exists(filepath):
-    filepath, _ = urllib.urlretrieve(SOURCE_URL + filename, filepath)
+    filepath, _ = urlretrieve(SOURCE_URL + filename, filepath)
     statinfo = os.stat(filepath)
     print('Succesfully downloaded', filename, statinfo.st_size, 'bytes.')
   return filepath
